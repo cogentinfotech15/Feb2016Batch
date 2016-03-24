a9c74b5e5290d30bd78410c8e5b3ba48173119f0
PRUDHVI RAJ <Prudhvi@Prudhvis-MacBook-Pro.local>
2016-03-24 14:16:22 -0400

    TestChange
    Comment has been written

--- src/main/java/com/prudhvi/myapp/HomeController.java
+++ src/main/java/com/prudhvi/myapp/HomeController.java
@@ -11,6 +11,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+/*
+ * I am writing a comment*/
 /**
  * Handles requests for the application home page.
  */
